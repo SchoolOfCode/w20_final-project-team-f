c31b129eb3a07d210fadf87b1b1379a4ac067208
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-08-24 02:15:30 +0100

    navbar-routing and homepage

--- nutri-glow/src/App.js
+++ nutri-glow/src/App.js
@@ -1,7 +1,30 @@
 import './app.scss';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import Articles from './pages/Articles';
+import Recipes from './pages/Recipes';
+import Profile from './pages/Profile';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="container">
+      <Router>
+        <nav>
+          <Navbar />
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/articles" component={Articles} />
+          <Route path="/recipes" component={Recipes} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ nutri-glow/src/components/Navbar/MenuList.js
@@ -0,0 +1,14 @@
+export const MenuList = [
+  {
+    title: 'Track your health',
+    url: '/profile',
+  },
+  {
+    title: 'Articles',
+    url: '/articles',
+  },
+  {
+    title: 'Recipes',
+    url: '/recipes',
+  },
+];
