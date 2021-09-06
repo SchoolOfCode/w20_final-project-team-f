be84bf10469428c44205f1566f34d1bf2cd4e551
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-09-06 16:05:25 +0100

    styled login page component

--- nutri-glow/client/src/components/Login/Login.js
+++ nutri-glow/client/src/components/Login/Login.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import { auth } from '../../firebase';
-import './Login.scss'
-import Signup from '../Signup/Signup'
-import { Link } from 'react-router-dom'
+import './Login.scss';
+import Signup from '../Signup/Signup';
+import { Link } from 'react-router-dom';
 
 // form to capture user credential
 // when user fills form, login and access is given to profile page
@@ -10,49 +10,74 @@ import { Link } from 'react-router-dom'
 // if user doesn't have a pre-existing account, provide option to sign up
 
 function Login() {
-
   // capture user input value for later use
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const signUp = e => {
+  const signUp = (e) => {
     e.preventDefault();
-    auth.createUserWithEmailAndPassword(
-      emailRef.current.value,
-      passwordRef.current.value
-    ).then(user => {
-      console.log(user)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  const signIn = e => {
+  const signIn = (e) => {
     e.preventDefault();
-    auth.signInWithEmailAndPassword(
-      emailRef.current.value,
-      passwordRef.current.value
-    ).then(user => {
-      console.log(user)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div className="signin">
-      <form action="">
-        <h1>Sign in</h1>
-        <label>Email</label>
-        <input ref={emailRef} type="email" />
-        <label>Password</label>
-        <input ref={passwordRef} type="password" />
-        <button onClick={signIn}>Sign in </button>
-        <h6>Not yet registered? <Link to="/signup" className="btn btn-primary">Sign Up</Link></h6>
-      </form>
-
+    <div className="loginContainer">
+      <div className="leftLogin">
+        <div className="loginWrapper">
+          <div className="loginHeader">
+            <h1>Sign in</h1>
+          </div>
+          <div className="loginFormContainer">
+            <form action="" className="loginForm">
+              <div className="emailLogin">
+                <label className="loginLabel">Email</label>
+                <input ref={emailRef} type="email" />
+              </div>
+              <div className="passwordLogin">
+                <label className="loginLabel">Password</label>
+                <input ref={passwordRef} type="password" />
+              </div>
+              <button className="loginButton" onClick={signIn}>
+                Sign in{' '}
+              </button>
+              <h4>
+                Not yet registered? <Link to="/signup">Sign Up</Link>
+              </h4>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="rightLogin">
+        <div className="loginImageContainer">
+          <img src="/assets/signup.png" className="loginImage" alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
