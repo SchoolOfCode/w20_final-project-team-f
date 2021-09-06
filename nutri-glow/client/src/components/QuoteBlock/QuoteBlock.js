dc8273dc86ed6d7c6f79b527db5670063cb5608a
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-09-06 09:19:30 +0100

    image showing next to quote

--- nutri-glow/client/src/components/QuoteBlock/QuoteBlock.js
+++ nutri-glow/client/src/components/QuoteBlock/QuoteBlock.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import "./QuoteBlock.scss"
-import "./preg-image.png"
+import React from 'react';
+import './QuoteBlock.scss';
 
 const QuoteBlock = (props) => {
-    return (
-        <div className="quoteBlockContainer">
-            <div className="quoteBlockLeft">
-                <img className="quoteBlockImage" src="./preg-image.png"/>
-            </div>
-            <div className="quoteBlockRight">
-                <h2 className="quoteText">{props.text}</h2>
-                <br/>
-                <h2 className="quoteAuthor">{props.author}</h2>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="quoteBlockContainer">
+      <div className="quoteBlockLeft">
+        <img className="quoteBlockImage" src="/assets/pregImge.png" alt="" />
+      </div>
+      <div className="quoteBlockRight">
+        <h2 className="quoteText">{props.text}</h2>
+        <br />
+        <h2 className="quoteAuthor">{props.author}</h2>
+      </div>
+    </div>
+  );
+};
 
-export default QuoteBlock
-
+export default QuoteBlock;
--- nutri-glow/client/src/pages/Profile.js
+++ nutri-glow/client/src/pages/Profile.js
@@ -1,76 +1,71 @@
-
 import React from 'react';
 import ProfileIntro from '../components/ProfileIntro/ProfileIntro';
-import { useLayoutEffect } from 'react';
-import { useState, useEffect }  from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import RecipesButton from '../components/RecipesButton/RecipesButton';
 import RecRecipeCard from '../components/RecRecipeCard/RecRecipeCard';
 import QuoteBlock from '../components/QuoteBlock/QuoteBlock';
 import './Profile.scss';
 
-
 export default function Profile() {
+  const [mealType, setMealType] = useState('Dinner');
 
-const [mealType, setMealType] = useState('Dinner');
+  const query = 'tasty';
 
-const query = "tasty";
-  
-const APP_ID = '143e5a61';
-const APP_KEY = '1aba1110ee2c42dcaaeccce62c1f3e22';
+  const APP_ID = '143e5a61';
+  const APP_KEY = '1aba1110ee2c42dcaaeccce62c1f3e22';
 
- const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-useEffect(() => {
-  getRecipes();
+  useEffect(() => {
+    getRecipes();
+  }, [mealType]);
 
-}, [mealType]);
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${mealType}`
+      // `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&to=6`
+    );
 
-const getRecipes = async () => {
-  const response = await fetch(
-    `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${mealType}`
-       // `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&to=6`
-  );
-
-  const recipeData = await response.json();
-  // all data is saved in the state
-  setRecipes(recipeData.hits);
-};
+    const recipeData = await response.json();
+    // all data is saved in the state
+    setRecipes(recipeData.hits);
+  };
 
   return (
-<div>
-  <div>
-    <ProfileIntro />
-  </div>
-  <div className="wrapper">
-    <div className="profileQuote">
-    <QuoteBlock
-      text='"In giving birth to our babies, we may find that we give birth to new possibilities within ourselves."'
-      author="- Myla and Jon Kabat-Zinn"
-    />
-    </div>
-    <div className="recRecipes">
-      <h1 className="recRecipesTitle">Recommended <br/> recipes</h1>
-      <div className="recRecipesCards">
-        {recipes.slice(0, 3).map((recipe) => (
-          <RecRecipeCard
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories.toFixed()}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-            url={recipe.recipe.url}
+    <div>
+      <div>
+        <ProfileIntro />
+      </div>
+      <div className="wrapper">
+        <div className="profileQuote">
+          <QuoteBlock
+            text='"In giving birth to our babies, we may find that we give birth to new possibilities within ourselves."'
+            author="- Myla and Jon Kabat-Zinn"
           />
-        ))}
-      </div>
-      <div className="recRecipesButton">
-      <Link to="/recipes">
-        <RecipesButton
-          name="Discover all"
-        />
-      </Link>
+        </div>
+        <div className="recRecipes">
+          <h1 className="recRecipesTitle">
+            Recommended <br /> recipes
+          </h1>
+          <div className="recRecipesCards">
+            {recipes.slice(0, 3).map((recipe) => (
+              <RecRecipeCard
+                title={recipe.recipe.label}
+                calories={recipe.recipe.calories.toFixed()}
+                image={recipe.recipe.image}
+                ingredients={recipe.recipe.ingredients}
+                url={recipe.recipe.url}
+              />
+            ))}
+          </div>
+          <div className="recRecipesButton">
+            <Link to="/recipes">
+              <RecipesButton name="Discover all" />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-)
+  );
 }
