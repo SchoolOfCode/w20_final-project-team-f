76e306eb879e8e1d8cd5ca9e29ef6b51c105c820
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-09-07 21:35:35 +0100

    added title for trimester cards

--- nutri-glow/client/src/components/TrackerCategories/TrackerCategories.js
+++ nutri-glow/client/src/components/TrackerCategories/TrackerCategories.js
@@ -5,19 +5,24 @@ import { Link } from 'react-router-dom';
 
 export default function TrackerCategories() {
   return (
-    <div className="trimesterCardContainer">
-      {trimesterCard.map((data) => (
-        <Link exact to={data.url}>
-          <div className="itemCardContainer">
-            <div className="cardContainerImage">
-              <img className="cardImage" src={data.img} alt="" />
+    <div className="trimesterIntro">
+      <div className="trimesterTitle">
+        <h1>Find out what your trimester means for you</h1>
+      </div>
+      <div className="trimesterCardContainer">
+        {trimesterCard.map((data) => (
+          <Link exact to={data.url}>
+            <div className="itemCardContainer">
+              <div className="cardContainerImage">
+                <img className="cardImage" src={data.img} alt="" />
+              </div>
+              <div className="textCardContainer">
+                <h3>{data.title}</h3>
+              </div>
             </div>
-            <div className="textCardContainer">
-              <h3>{data.title}</h3>
-            </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
